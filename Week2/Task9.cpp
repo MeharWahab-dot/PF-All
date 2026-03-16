ff0d322c7e2279c8a4073567d7a704a37ed956e1
MeharWahab-dot <meharwahab688@gmail.com>
2026-03-16 16:12:59 +0500

    Add files via upload

---
+++ Week2/Task9.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main(){
+cout<<"   ---------------------------    \n";                      
+cout<<"       o    ^__^                  \n";
+cout<<"         o  (oo)\\_________       \n";         
+cout<<"\n";        
+cout<<"            (__)\\       )\\/\\   \n";
+cout<<"               ||------w |        \n";      
+cout<<"               ||   .  || .       \n";      
+} 
+               
+                                                  
